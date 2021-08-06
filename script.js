ac40eaef1ed293356583486340fc94c9ff96ae6a
gerardojao <gerardojao@gmail.com>
2021-08-06 17:43:32 -0430

    funcional menu haburguesa #000

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const panelBtn = d.getElementById("panel-btn")
 const navBarContainer = d.querySelector(".navbar__container")
 const menu = d.querySelector(".menu")
 const $scrollBtn = d.querySelector(".scroll-top-btn")
-/* const icon = d.querySelector(".fas") */
+
 const inputs = document.querySelectorAll(".main [required]")
 const form = document.querySelector(".main__form")
 const mensaje =document.getElementById("mensaje")
@@ -21,39 +21,25 @@ const close = d.querySelector(".close-btn")
 
 const pModal=d.querySelector(".pattern")
 
-/* menu hamburguesa
-
-export default function hambMenu(panelBtn, panel,menu){
-    const d = document;
-   //  const panelId = d.getElementById("panel-btn") 
-    //hambMenu(".panel-btn",".panel",".link")
-
-   d.addEventListener("click",e=>{
-    if(e.target.matches(panelBtn)||e.target.matches(`${panelBtn} *`)){
-        d.querySelector(panel).classList.toggle("is-active")
-        d.querySelector(panelBtn).classList.toggle("is-active")
-    }
-    if(e.target.matches(menu)){
-        d.querySelector(panel).classList.remove("is-active")
-        d.querySelector(panelBtn).classList.remove("is-active")
-    }
-})
-
-}
-*/
-
+/* menu hamburguesa*/
 
 
 panelBtn.addEventListener("click",()=>{
     menu.classList.toggle("menu--active")
-/*     icon.classList.toggle("active") */
+   panelBtn.classList.toggle("is-active")
+
+    linkMenu.forEach(x=>{
+        menu.addEventListener("click",e=>{
+           if(e.target.offsetParent.classList.contains("menu--active")){
+               e.target.offsetParent.classList.remove("menu--active")
+               panelBtn.classList.toggle("is-active")
+            }
+        })
+    })
+
 })
 
-linkMenu.forEach(x=>{
-    menu.addEventListener("click",e=>{
-       if(e.target.offsetParent.classList.contains("menu--active"))e.target.offsetParent.classList.remove("menu--active")
-    })
-})
+
 
 w.addEventListener("scroll",()=>{
     if(this.scrollY > 30){
@@ -67,37 +53,40 @@ w.addEventListener("scroll",()=>{
         link.forEach(x=>x.classList.replace("navbar__link--white","navbar__link--a"))
     }
 
-    if(this.scrollY>500) $scrollBtn.classList.remove("hidden")
+    if(this.scrollY>500){
+        $scrollBtn.classList.remove("hidden")
+        $scrollBtn.addEventListener("click",()=>{
+            w.scrollTo({
+                        behavior:"smooth",
+                        top:0
+                      })
+             })
+    }
     else $scrollBtn.classList.add("hidden")
 
 })
 
 
-$scrollBtn.addEventListener("click",()=>{
-    w.scrollTo({
-                behavior:"smooth",
-                top:0
-              })
-     })
-    
 mensaje.addEventListener("keyup",()=>{
 
- if(mensaje.nextElementSibling.classList.contains("mensaje")){
-    mensaje.nextElementSibling.classList.replace("mensaje","mensaje-js")}
- 
- if(mensaje.value.length<1){
-    mensaje.nextElementSibling.classList.replace("mensaje-js","mensaje")}
- })
-
-
-for (let i = 0; i < inputs.length; i++)
-     inputs[i].addEventListener("keyup",()=>{
-    if(inputs[i].nextElementSibling.classList.contains("form-label"))
-        inputs[i].nextElementSibling.classList.replace("form-label","form-js")
-    if (inputs[i].value.length<1) 
-        inputs[i].nextElementSibling.classList.replace("form-js","form-label")
+    if(mensaje.nextElementSibling.classList.contains("mensaje")) mensaje.nextElementSibling.classList.replace("mensaje","mensaje-js")
     
+    if(mensaje.value.length<1) mensaje.nextElementSibling.classList.replace("mensaje-js","mensaje")
     })
+   
+   
+   for (let i = 0; i < inputs.length; i++)
+        inputs[i].addEventListener("keyup",()=>{
+       if(inputs[i].nextElementSibling.classList.contains("form-label"))
+           inputs[i].nextElementSibling.classList.replace("form-label","form-js")
+       if (inputs[i].value.length<1) 
+           inputs[i].nextElementSibling.classList.replace("form-js","form-label")
+       
+       })
+
+
+    
+
 
 d.addEventListener("submit",e=>{
 
@@ -121,6 +110,7 @@ d.addEventListener("submit",e=>{
         content.classList.remove("content__hide")
         pModal.innerHTML =` ${inputs[0].value}`
         //console.log("hola")
+        
     })
     .catch(err=>{
         console.log(err)
